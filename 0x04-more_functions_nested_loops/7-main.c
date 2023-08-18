40b707ebb13e0a12e56c18952ccc9278da7a4044
ZeeZyy <zeezyymoahmed71@gmail.com>
2023-08-18 13:23:25 -0700

    0

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+rint_diagonal(0);
+print_diagonal(2);
+print_diagonal(10);
+print_diagonal(-4);
+return (0);
+}
